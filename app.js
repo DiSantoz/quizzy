232e885ad3ccfa9efd41db9c10702cced96d1e71
David Ferreira <dsferreira06@gmail.com>
2022-04-19 20:21:20 -0400

    verify users answers

---
+++ app.js
@@ -0,0 +1,30 @@
+// place correct answers in array
+const correctAnswer = ["B", "B", "B", "B"];
+
+const form = document.querySelector(".quiz-form");
+
+// when submit is clicked, verify user's answers
+form.addEventListener("submit", (e) => {
+  // prevent's page refreshing upon click
+  e.preventDefault();
+
+  //   user score defaults to 0
+  let score = 0;
+
+  //   users answers are place in array
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+
+  //   check users answers against correct answer
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswer[index]) {
+      score += 25;
+    }
+  });
+
+  console.log(score);
+});
